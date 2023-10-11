8a4174c30f514ef2ffe63b0464edd0c66853860a
Benett Viszokai <145262915+benettviszokai@users.noreply.github.com>
2023-10-11 11:29:11 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,62 @@
+using System;
+using System.CodeDom.Compiler;
+
+namespace probaprobaproba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            
+            */
+            // Eredeti string
+            string message = "HELLOW ORLD!";
+
+            // Eredeti string hossza
+            int allCharacters = message.Length; // 12
+
+            // Hol a szóköz? (+ milyen hosszú, hátha kell)
+            int space = message.IndexOf(' '); // 6 -> 6. indexen van (7. karakter)
+            string spacestring = space.ToString();
+            int spaceLength = spacestring.Length;
+
+            // Szétbontjuk a stringet
+            string part1 = message.Substring(0, space); // "HELLOW"
+            string part2 = message.Substring(space + 1); // "ORLD!"
+
+            // A szétbontott stringek hossza
+            int part1length = part1.Length; // 6 (HELLOW)
+            int part2length = part2.Length; // 5 (ORLD!)
+
+            // Megcseréljük a szétbontott stringek hosszát -> part1 = 5, part2 = 6
+            int temp = 0;
+            temp = part1length; // 6
+            part1length = part2length; // 5
+            part2length = temp; // 6 
+
+            // Elkészítjük az új part 1-et -> cél: HELLOW -> HELLO
+            char[] newPart1 = new char[part1length];
+
+            for (int i = 0; i < part1length; i++)
+            {
+                newPart1[i] = part1[i]; // HELLO -> ez eddig jó
+            }
+
+            // Elkészítjük az új part 2-t -> cél: ORLD! -> WORLD!
+
+            // Ötlet: eredeti stringből kivenni a szóközt, készíteni egy új stringet, hátha segít a második szóban
+            string trimmedMessage = message.Replace(" ", "");
+            int trimmedMessageLength = trimmedMessage.Length;
+            Console.WriteLine(trimmedMessageLength);
+            
+            char[] newPart2 = new char[part2length];
+
+            for(int i = part1length+1; i < trimmedMessageLength+1; i++)
+            {
+                Console.WriteLine(trimmedMessage[i]);
+            }
+            
+        }
+    }
+}
